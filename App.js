51d096bbc94cfba455374bfc651f27897ae95d7e
coaudtn0276 <coaudtn0276@gmail.com>
2023-08-28 23:04:36 +0900

    input

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from "react-native";
+import { theme } from "./color";
+import { useState } from "react";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => {
+    setText(payload);
+  };
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    const newTodos = Object.assign({}, toDos, { [Date.now()]: { text, work: working } });
+    setToDos(newTodos);
+    setText("");
+  };
+
+  console.log(toDos);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? "white" : theme.grey }}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !working ? "white" : theme.grey }}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput onSubmitEditing={addToDo} returnKeyType="done" value={text} onChangeText={onChangeText} placeholder={working ? "할 일을 추가 하세요!" : "어디 가고 싶으신가요?"} style={styles.input} />
     </View>
   );
 }
@@ -13,8 +43,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
